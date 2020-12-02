7bf2786ad3c1f9ae36807908529ca04006066f0e
Aurel <aurel@space.mynet>
2020-12-02 19:21:08 +0000

    A simple pokemon retriever with pagination.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import PokemonList from "./PokemonList";
+import axios from "axios";
+import Pagination from "./Pagination";
 
-function App() {
+export default function App() {
+  const [pokemon, setPokemon] = useState(["aurel"]);
+  const [currPageUrl, setCurrPageUrl] = useState(
+    "https://pokeapi.co/api/v2/pokemon"
+  );
+  const [loading, setLoading] = useState(true);
+
+  const [nextPageUrl, setNextPageUrl] = useState();
+  const [prevPageUrl, setPrevPageUrl] = useState();
+
+  useEffect(() => {
+    setLoading(true);
+    let cancel;
+    axios
+      .get(currPageUrl, {
+        cancelToken: new axios.CancelToken((c) => (cancel = c)),
+      })
+      .then((res) => {
+        setLoading(false);
+        setNextPageUrl(res.data.next);
+        setPrevPageUrl(res.data.previous);
+        setPokemon(res.data.results.map((p) => p.name));
+      });
+
+    return () => {
+      cancel();
+    };
+  }, [currPageUrl]);
+
+  function gotoNextPage() {
+    setCurrPageUrl(nextPageUrl);
+  }
+
+  function gotoPrevPage() {
+    setCurrPageUrl(prevPageUrl);
+  }
+
+  if (loading) return "Loading...";
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <PokemonList pokemon={pokemon} />
+      <Pagination
+        gotoNextPage={nextPageUrl ? gotoNextPage : null}
+        gotoPrevPage={prevPageUrl ? gotoPrevPage : null} />
+    </>
   );
 }
-
-export default App;
